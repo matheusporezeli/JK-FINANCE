f00218b0a2517b4ada1200619e17ce86dd7896fc
Matheus Porezeli <matheusporezeli@gmail.com>
2025-10-31 20:28:22 -0300

    31-10
    Adição do menu hamburguer

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Função alterar classes de estilização do nav-mobile
+
+function menuOnClick() {
+    document.getElementById("menu").classList.toggle("menu-change");
+  document.getElementById("menu-bar").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+}
